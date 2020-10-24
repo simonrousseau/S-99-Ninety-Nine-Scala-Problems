ddf762770baca44f86a0facf5477cc99a377ae3d
simonrousseau <simonrousseau22@gmail.com>
2020-10-24 13:48:32 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "S-99-Ninety-Nine-Scala-Problems"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
---
+++ src/main/scala/solution/Main.scala
@@ -0,0 +1,6 @@
+package solution
+
+object Main extends App {
+  val ages = Seq(42, 75, 29, 64)
+  println(s"The oldest person is ${ages.max}")
+}
